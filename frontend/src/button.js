7da45b629191a482cb6112ffd252176e42a4256d
KevOdo <kevin@odas.it>
2022-11-08 20:18:37 +0100

    Added authentication

--- frontend/src/button.js
+++ frontend/src/button.js
@@ -1,14 +1,30 @@
 const CMS_URI = "//localhost:1337/api/";
 
-const POST_IMAGE = data => API_POST("upload", data);
-const GET_IMAGE = () => API_GET("galleries")
+const POST_IMAGE = (token) => AUTH_POST("upload", token);
+const GET_TOKEN = code => AUTH_GET("auth/local", code);
 
-async function API_POST(url, data) {
+async function AUTH_POST(url, token) {
     const form = new FormData();
     const selectedFile = document.getElementById('image').files[0];
     form.append('files', selectedFile)
     try {
         console.log(form)
+        const response = await axios.post(CMS_URI + url, form, {
+            headers: {
+                Authorization:`Bearer ${token}`
+        }});
+        return response;
+    } catch (error) {
+        return error;
+    }
+}
+
+async function AUTH_GET(url, code) {
+    const form = {
+        identifier: "mat@strapi.io",
+        password: code
+    }
+    try {
         const response = await axios.post(CMS_URI + url, form);
         return response;
     } catch (error) {
@@ -16,16 +32,9 @@ async function API_POST(url, data) {
     }
 }
 
-async function API_GET(url) {
-    try {
-        const response = await axios.get(CMS_URI + url);
-        return response;
-    } catch (error) {
-        return error;
-    }
-}
-
-function activate(code, image) {
-    console.log("CODE: " + code.value);
-    //POST_IMAGE(image).then(response => console.log(response));
+function activate(code) {
+    console.log(code.value)
+    GET_TOKEN(code.value).then(response => {
+        POST_IMAGE(response.data.jwt).then(res => console.log(res))
+    })
 }
