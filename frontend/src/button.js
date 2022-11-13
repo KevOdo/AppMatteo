8dbf48fbd6f4f7f861faa64586bcfd2593320793
KevOdo <kevin@odas.it>
2022-11-13 15:41:45 +0100

    Style changes + added image renaming

--- frontend/src/button.js
+++ frontend/src/button.js
@@ -1,15 +1,12 @@
-const CMS_URI = "//192.168.0.18:1337/api/";
+const CMS_URI = "//formula3e14.redirectme.net:1337/api/";
 
-const POST_IMAGE = (token, destination) => AUTH_POST("upload/" + destination, token);
+const POST_IMAGE = (token, image) => AUTH_POST("upload", token, image);
 const GET_TOKEN = code => AUTH_GET("auth/local", code);
 
-async function AUTH_POST(url, token,) {
+async function AUTH_POST(url, token, image) {
     const form = new FormData();
-    const selectedFile = document.getElementById('image').files[0];
-    console.log(selectedFile.type)
-    form.append('files', selectedFile)
+    form.append('files', image)
     try {
-        console.log(form)
         const response = await axios.post(CMS_URI + url, form, {
             headers: {
                 Authorization:`Bearer ${token}`
@@ -33,14 +30,40 @@ async function AUTH_GET(url, code) {
     }
 }
 
+async function SEND_SERVER() {
+    try {
+        const response = await axios.post("./", "hello");
+        return response;
+    } catch (error) {
+        return error;
+    }
+}
+
+const createFile = (bits, name, options) => {
+    try {
+        // If this fails, we go for Blob
+        return new File(bits, name, options);
+    } catch (e) {
+        // If we're here a new File could not be constructed
+        var myBlob = new Blob(bits, options || {});
+        myBlob.lastModified = new Date();
+        myBlob.name = name;
+        return myBlob;
+    }
+};
+
 function activate(code, destination) {
     const selectedFile = document.getElementById('image').files[0];
+    tmp = destination.value + selectedFile.name;
+
+    const image = createFile([selectedFile], tmp, {type: selectedFile.type});
+
     if(selectedFile.type.includes("image")) {
         GET_TOKEN(code.value).then(response => {
-            console.log(destination.value)
-            POST_IMAGE(response.data.jwt, destination.value).then(res => console.log(res))
+            POST_IMAGE(response.data.jwt, image).then(res => console.log(res))
         })
     } else {
         console.log("NOT AN IMAGE")
+        //SEND_SERVER().then(response => console.log("response: " + response));  
     }
 }
--- frontend/webapp.js
+++ frontend/webapp.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const events = require('events');
 const app = express();
 const hostname = "localhost";
 const port  = process.env.PORT || 8888;
+
+const eventEmitter = new events.EventEmitter();
 
 app.use("/src", express.static(__dirname + "/src"));
 
@@ -10,6 +13,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"./src/main.html"));
 })
 
+app.get('/hello', (req, res) => {
+    console.log(req);
+    console.log(res);
+})
+
 app.listen(port, () => {
     console.log("Server start")
+    console.log("Listening on port: " + port);
 })
