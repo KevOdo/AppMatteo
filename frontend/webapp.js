fee1a5ee3564384e5bc98b80c1e497ccdccb015a
KevOdo <kevin@odas.it>
2022-11-13 20:07:34 +0100

    Style changes + folder routing

--- frontend/webapp.js
+++ frontend/webapp.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
-const events = require('events');
 const app = express();
+const fs = require('fs');
+
+const absolutePath = path.resolve("");
+const oldPath = absolutePath.replace("/frontend", "/backend/app-matteo/public/uploads/");
+const newPath = absolutePath.replace("AppMatteo/frontend", "Immagini/");
+const nm = "salzano.txt";
+
 const hostname = "localhost";
 const port  = process.env.PORT || 8888;
-
-const eventEmitter = new events.EventEmitter();
 
 app.use("/src", express.static(__dirname + "/src"));
 
@@ -13,12 +17,21 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"./src/main.html"));
 })
 
-app.get('/hello', (req, res) => {
-    console.log(req);
-    console.log(res);
-})
-
 app.listen(port, () => {
     console.log("Server start")
     console.log("Listening on port: " + port);
+})
+
+fs.watch(oldPath, (eventType, filename) => {
+    if(eventType == "rename") {
+        if(filename.startsWith("salzano")) {
+            fs.rename((oldPath + filename), (newPath + "salzano/" + filename), function (err) {
+                if (err) console.log(error);
+            })
+        } else if (filename.startsWith("zerobranco")) {
+            fs.rename((oldPath + filename), (newPath + "zerobranco/" + filename), function (err) {
+                if (err) console.log(error);
+            })
+        }
+    }
 })
